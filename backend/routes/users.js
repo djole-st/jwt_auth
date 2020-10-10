6030dad5b2260844f8588f3db9777a84bb2275fd
Djordje Stankovic <djordje.stankovic87@gmail.com>
2020-10-11 00:16:59 +0200

    Frontend setup, backend registration finished

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', function(req,res,next){
+  var new_user = new User({
+    email: req.body.email,
+    username: req.body.username,
+    password: User.hashPassword(req.body.password),
+    creation_dt: Date.now()
+  });
+
+  let promise = new_user.save();
+
+  promise.then(function(doc){
+    return res.status(201).json(doc);
+  })
+
+  promise.catch(function(err){
+    return res.status(501).json({message: 'Error registering user.'});
+  })
+
 });
 
 module.exports = router;
